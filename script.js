ebb453105ad744bc704a69fed94908e8f7966a1f
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-16 11:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
 class Animal {}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		this._species = species
+	}
+	get species(){
+	return this._species;
+	}
+	macksound(){
+		console.log(`the ${._species}`makes a sound)
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr() {
+    console.log("purr");
+  }
+}
+class Dog extends Animal {
+  // Method specific to Dog
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
